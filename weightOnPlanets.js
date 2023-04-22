238458a81757cd4dcc071becfef39de2a72d29d9
Sandhya80 <129001012+Sandhya80@users.noreply.github.com>
2023-04-22 04:43:12 +0100

    Initial commit

---
+++ weightOnPlanets.js
@@ -0,0 +1,22 @@
+//Calculating weight of objects on different planets
+
+const calculateWeight = (earthWeight, planet) =>  {
+  switch (planet)  {
+    case 'Mercury':
+    return earthWeight * 0.378;    
+    case 'Venus':
+    return earthWeight * 0.907;    
+    case 'Mars':
+    return earthWeight * 0.377;    
+    case 'Jupiter':
+    return earthWeight * 2.36;    
+    case 'Saturn':
+    return earthWeight * 0.916;
+    default:
+    return 'Invalid Planet Entry. Try: Mercury, Venus, Mars, Jupiter, or Saturn.';
+    break;    
+ } 
+};
+
+console.log(calculateWeight(100, 'Jupiter')) // Should print 236
+console.log(calculateWeight(67,'Mars')) // Should print 25.259
